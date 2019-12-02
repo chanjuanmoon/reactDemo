f4c1a38aeb573eb936577f03efd8e4a261f2b8fb
chanjuan <liuq@jrtechsoft.com.cn>
2019-12-02 18:33:19 +0800

    create-react-app

---
+++ basicDemo/08/demo/src/components/demo.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+export default class Demo extends Component {
+  render() {
+    return (
+      <div>
+        <span>123</span>
+      </div>
+    )
+  }
+}
+
+
+
+
