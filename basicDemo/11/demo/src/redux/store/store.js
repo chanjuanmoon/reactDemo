f532ed4640d3f6e8547ff6d91c3e50ded3bc7db3
chanjuan <liuq@jrtechsoft.com.cn>
2019-12-05 10:17:03 +0800

    使用Redux调试工具

--- basicDemo/11/demo/src/redux/store/store.js
+++ basicDemo/11/demo/src/redux/store/store.js
@@ -1,12 +1,13 @@
 import {createStore,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 import {counter} from '../reducers/reducers'
 
 // 生成一个store对象
 const store = createStore(
   counter,
-  applyMiddleware(thunk)//应用上异步中间件
+  composeWithDevTools(applyMiddleware(thunk))//应用上异步中间件
   );//内部会第一次调用reducer函数，得到初始state
 
 export default store;
